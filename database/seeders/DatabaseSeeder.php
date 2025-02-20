5c30258906f2784d1d2b5ec3f97d6bf774d54d46
AceReX <ace.rexnoir@gmail.com>
2025-02-20 21:05:38 +0530

    Add Category model, migration, factory, controller, and seeder for category management

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             ShieldSeeder::class,
             RoleSeeder::class,
             UserSeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
