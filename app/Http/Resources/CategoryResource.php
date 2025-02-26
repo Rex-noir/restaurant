b2d299780d1559a08fa7e2491819409725e79022
AceReX <ace.rexnoir@gmail.com>
2025-02-26 19:23:36 +0530

    feat : add CategoryResource and MenuItemResource for API responses

---
+++ app/Http/Resources/CategoryResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'description' => $this->description,
+            'image' => asset($this->image_path),
+            'menu_items' => MenuItemResource::collection($this->menuItems),
+        ];
+    }
+}
