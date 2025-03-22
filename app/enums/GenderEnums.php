2f6d6d2720665ed487e6d83bc78075605b25b978
AceReX <ace.rexnoir@gmail.com>
2025-03-22 20:58:50 +0530

    feat: add Unknown gender option to GenderEnums
    This commit adds an "unknown" option to the `GenderEnums` enum. This provides a more inclusive and accurate representation of gender options. The `options()` method is updated to include the new option.

--- app/enums/GenderEnums.php
+++ app/enums/GenderEnums.php
@@ -2,22 +2,23 @@
 
 namespace App\enums;
 
-enum GenderEnums: String
+enum GenderEnums: string
 {
     case Male = 'male';
     case Female = 'female';
     case Other = 'other';
+    case Unknown = 'unknown';
 
     public static function values(): array
     {
-        return array_map(fn($case) => $case->value, self::cases());
+        return array_map(fn ($case) => $case->value, self::cases());
     }
 
     public static function options()
     {
         return array_combine(
-            array_map(fn(GenderEnums $case) => $case->value, self::cases()),
-            array_map(fn(GenderEnums $case) => $case->name, self::cases())
+            array_map(fn (GenderEnums $case) => $case->value, self::cases()),
+            array_map(fn (GenderEnums $case) => $case->name, self::cases())
         );
     }
 }
