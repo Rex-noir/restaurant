9a4abab2992d194ccf43aa2e470f0e1f7ee2b828
AceReX <ace.rexnoir@gmail.com>
2025-03-22 14:44:06 +0530

    feat: allow null date of birth in UserProfileData
    This commit updates the `UserProfileData` data object to allow for a null `dateOfBirth` value. This change provides flexibility in handling user profile data where the date of birth may not always be available.

--- app/Data/UserProfileData.php
+++ app/Data/UserProfileData.php
@@ -13,6 +13,7 @@ class UserProfileData extends Data
     public function __construct(
         public GenderEnums $gender,
         public string $phone,
+        
         public ?string $dateOfBirth,
         public int|Optional $userId,
         public ?ImageData $profileImage
