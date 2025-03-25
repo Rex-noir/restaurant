74932e1673484fb40d48163e0b04de8331f2d993
AceReX <ace.rexnoir@gmail.com>
2025-03-25 23:28:28 +0530

    feat: Create InputGroup component
    This commit introduces a new `InputGroup` component to encapsulate the common structure of input fields, including a label, input element, and error message display. This component enhances code reusability and consistency across the application. The component accepts props for the label, placeholder, input type, and error message, and uses `v-model` to bind to the input value. This change simplifies the creation and management of input fields throughout the application.

---
+++ resources/js/Components/input-group.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const inputText = defineModel('inputText', { required: true });
+
+defineProps<{
+    label: string;
+    placeholder?: string;
+    inputType?: string;
+    error?: string;
+}>();
+</script>
+
+<template>
+    <div v-bind="$attrs" class="form-control w-full">
+        <label class="label">
+            <span class="label-text">{{ label }}</span>
+        </label>
+        <input
+            v-model="inputText"
+            :type="inputType ?? 'text'"
+            :placeholder="placeholder"
+            class="input w-full focus-within:outline-none"
+            :class="{ 'input-error': error }"
+        />
+        <label v-if="error" class="label">
+            <span class="label-text-alt text-error">{{ error }}</span>
+        </label>
+    </div>
+</template>
