f4709a896017b9066384140a37a8006338539148
AceReX <ace.rexnoir@gmail.com>
2025-03-28 22:27:31 +0530

    feat: Add MenuItem resource with create, edit, and list pages

---
+++ app/Filament/Resources/MenuItemResource.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MenuItemResource\Pages;
+use App\Filament\Resources\MenuItemResource\RelationManagers;
+use App\Models\MenuItem;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class MenuItemResource extends Resource
+{
+    protected static ?string $model = MenuItem::class;
+
+    protected static ?int $navigationSort = 0;
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('category_id')
+                    ->relationship('category', 'name')
+                    ->preload()
+                    ->native(false)
+                    ->searchable(),
+
+                TextInput::make('name')
+                    ->required(),
+                Textarea::make('description')
+                    ->columnSpanFull(),
+
+                TextInput::make('price')->prefix('₹')
+                    ->required(),
+                TextInput::make('preparation_time')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('category.name')
+                    ->alignCenter(),
+                TextColumn::make('price')
+                    ->money('INR'),
+                TextColumn::make('preparation_time')
+                    ->alignCenter()
+                    ->suffix(' min'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMenuItems::route('/'),
+            'create' => Pages\CreateMenuItem::route('/create'),
+            'edit' => Pages\EditMenuItem::route('/{record}/edit'),
+        ];
+    }
+}
